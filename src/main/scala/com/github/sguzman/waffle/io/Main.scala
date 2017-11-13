928f79b629c22a0f4ff818d43b82f92c5e02c499
Salvador Guzman <guzmansalv@gmail.com>
2017-11-13 07:10:54 -0800

    stub source and test files

---
+++ src/main/scala/com/github/sguzman/waffle/io/Main.scala
@@ -0,0 +1,5 @@
+package com.github.sguzman.waffle.io
+
+object Main {
+
+}
---
+++ src/test/scala/com/github/sguzman/waffle/io/MainTest.scala
@@ -0,0 +1,7 @@
+package com.github.sguzman.waffle.io
+
+import org.scalatest.FlatSpec
+
+class MainTest extends FlatSpec {
+
+}
